ccd9ab19eac944397540c3b0b169f2b89eb0f7b5
Dmitry Panov <panov73@gmail.com>
2025-04-25 16:35:27 -0700

    changing docker-compose.yml

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['853660505798.dkr.ecr.us-east-2.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-129-172-115.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
